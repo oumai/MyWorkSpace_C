b3eedfcd89ce27f20e5a1766d789e75872cdaeff
oumai <oumaiko@yahoo.com>
2019-01-21 14:54:20 +0800

    no message

--- MyWorkSpace_C语言/Sn = S1+S2+Sn的表达式/Sn = S1+S2+Sn的表达式/main.m
+++ MyWorkSpace_C语言/Sn = S1+S2+Sn的表达式/Sn = S1+S2+Sn的表达式/main.m
@@ -8,35 +8,73 @@
 
 #import <Foundation/Foundation.h>
 
+void SnForAndFor(void);
+void SnFor(void);
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
         
         
-        int Sn,c;
-        Sn = 0;
-        c = 1;
         
+        /**
+         循环嵌套
+         */
+        SnForAndFor();
         
-        for (int i = 1; i<5; i++)
-        {
-            NSLog(@"i = %d",i);
-            
-            for (int j = 1; j<=i; j++)
-            {
-                NSLog(@"j = %d",j);
-                
-                c = c*j;
-                
-                
-            }
-            Sn = Sn +c;
-            c = 1;
-            
-        }
-        
-        NSLog(@"Sn = %d",Sn);
+        /**
+         单个循环
+         */
+        SnFor();
+      
     }
     return 0;
+}
+
+void SnFor()
+{
+    
+    int c = 1;
+    int Sn = 0;
+    
+    
+    for (int i = 1; i<=4; i++)
+    {
+        c = c *i;
+        
+        Sn = Sn +c;
+        
+    }
+    NSLog(@"for 1 Sn = %d",Sn);
+
+    
+}
+
+void SnForAndFor()
+{
+    int Sn,c;
+    Sn = 0;
+    c = 1;
+    
+    int count = 0;
+    
+    for (int i = 1; i<=4; i++)
+    {
+        NSLog(@"i = %d",i);
+        
+        for (int j = 1; j<=i; j++)
+        {
+            NSLog(@"j = %d",j);
+            
+            c = c*j;
+            
+            count ++;
+        }
+        Sn = Sn +c;
+        c = 1;
+        
+    }
+    
+    NSLog(@"Sn = %d,\nCount = %d",Sn,count);
 }
